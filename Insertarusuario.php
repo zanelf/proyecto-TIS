fd350710a47361c35b01c8cd95b605e62db6148b
Aesedefe1 <67512673+Aesedefe1@users.noreply.github.com>
2026-06-17 09:38:42 -0600

    Add files via upload
    autentificar guarda el tipo de usuario - al crear funcionario o director se pregunta el ID_departamento en un siguiente formulario

--- Insertarusuario.php
+++ Insertarusuario.php
@@ -16,17 +16,36 @@
     if($tipo=="Administrador"){
         $consulta = "INSERT INTO administrador (ID_usuario)  VALUES ('$id')";
         $resultado = mysqli_query($conexionDB,$consulta);
+        header('Location: /xampp/php/funciones/AsignarDpto.php');
     }
     if($tipo=="Funcionario"){
         $consulta = "INSERT INTO Funcionario (ID_usuario)  VALUES ('$id')";
         $resultado = mysqli_query($conexionDB,$consulta);
+        echo '<form action="/xampp/php/funciones/AsignarDpto.php" method="POST">';
+            echo '<label class=form-label>Departamento del funcionario</label>';
+            echo '<input type="number" name="departamento" class="form-control">';
+            echo '<label class=form-label>USUARIO: '.$id.'</label>';
+            echo '<input type="hidden" name="usuario" value="'.$id.'" class="form-control" >';
+            echo '<label class=form-label>TIPO: '.$tipo.'</label>';
+            echo '<input type="hidden" name="tipo" value="Funcionario" class="form-control" >';
+            echo '<input type="submit" class="btn btn-success mt-4 w-100">';
+        echo '</form>';
+        //header('Location: /xampp/php/funciones/AsignarDpto.php');
     }
     if($tipo=="Director"){
         $consulta = "INSERT INTO director (ID_usuario)  VALUES ('$id')";
         $resultado = mysqli_query($conexionDB,$consulta);
+        echo '<form action="/xampp/php/funciones/AsignarDpto.php" method="POST">';
+            echo '<label class=form-label>Departamento del funcionario</label>';
+            echo '<input type="number" name="departamento" class="form-control">';
+            echo '<label class=form-label>USUARIO: '.$id.'</label>';
+            echo '<input type="hidden" name="usuario" value="'.$id.'" class="form-control" >';
+            echo '<label class=form-label>TIPO: '.$tipo.'</label>';
+            echo '<input type="hidden" name="tipo" value="Funcionario" class="form-control" >';
+            echo '<input type="submit" class="btn btn-success mt-4 w-100">';
+        echo '</form>';
+        //header('Location: /xampp/php/funciones/AsignarDpto.php');
     }
 
     
-
-    header('Location: usuario.php');
 ?>
--- navbar1.php
+++ navbar1.php
@@ -1,33 +1,42 @@
 <?php
+$opciones = ["index" => "index",'ciudadano' => "Ciudadanos",'solicitud' => "Solicitudes", "tipo_solicitud" => "Tipos de solicitud","usuario" => "Usuarios", "departamento" => "Departamentos","logout" => "Cerrar sesion"];
+$opcionesAdm = ["index" => "index",'ciudadano' => "Ciudadanos",'solicitud' => "Solicitudes", "departamento" => "Departamentos", "usuarios" => "Usuarios"];
+$opcionesFun = ["index" => "index",'ciudadano' => "Ciudadanos",'solicitud' => "Solicitudes"];
+$opcionesDir = ["index" => "index",'ciudadano' => "Ciudadanos",'solicitud' => "Solicitudes"];
+$opcionesDev = ["index" => "index",'ciudadano' => "Ciudadanos",'solicitud' => "Solicitudes"];
+
 echo '<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4 shadow">';
     echo '<div class="container-fluid">';
-        echo '<a href="index.php">';
-        echo '<button type="button">INDEX</button>';
-        echo '</a>';
-        echo '<a href="solicitud.php">';
-        echo '<button type="button">SOLICITUDES</button>';
-        echo '</a>';
-        echo '<a href="usuario.php">';
-        echo '<button type="button">usuarios</button>';
-        echo '</a>';
-        echo '<a href="ciudadano.php">';
-        echo '<button type="ciudadanosssss">CIUDADANOS</button>';
-        echo '</a>';
-        echo '<a href="departamento.php">';
-        echo '<button type="departamentos">departamentos</button>';
-        echo '</a>';
-        echo '<a href="tipo_solicitud.php">';
-        echo '<button type="tipo_solicitud">tipo_solicitud</button>';
-        echo '</a>';
+        
+        if($_SESSION["tipo"]=="Administrador"){
+            foreach($opcionesAdm as $opcion => $label){
+                echo '<a href="'.$opcion.'.php">';
+                    echo '<button type="button">'.$label.'</button>';
+                echo '</a>';
+            }
+        }
+        if($_SESSION["tipo"]=="Funcionario"){
+            foreach($opcionesAdm as $opcion => $label){
+                echo '<a href="'.$opcion.'.php">';
+                    echo '<button type="button">'.$label.'</button>';
+                echo '</a>';
+            }
+        }
+        if($_SESSION["tipo"]=="Director"){
+            foreach($opcionesAdm as $opcion => $label){
+                echo '<a href="'.$opcion.'.php">';
+                    echo '<button type="button">'.$label.'</button>';
+                echo '</a>';
+            }
+        }
+    echo '</div>';
+
         echo '<a href="encuesta.php">';
         echo '<button type="encuesta">encuesta</button>';
         echo '</a>';
         echo '<a href="administrador.php">';
         echo '<button type="administrador">administrador</button>';
         echo '</a>';
-        echo '<a href="logout.php">';
-        echo '<button type="button">Cerrar sesión</button>';
-        echo '</a>';
-    echo '</div>';
+    
 echo '</nav>';
 ?>
