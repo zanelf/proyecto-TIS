bbdbe244360c2846ec6b54f9cd9d16c722b67097
Jayaquelo <fvidela@ing.ucsc.cl>
2026-06-18 00:40:55 -0400

    Cambio de nombre de título
    Municipalidad a SGISC

--- web-app/ventanas/Enviarsolicitud.php
+++ web-app/ventanas/Enviarsolicitud.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nueva Solicitud — Municipalidad</title>
+    <title>Nueva Solicitud — SGISC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="../recursos/css/style_enviar_solicitud_formulario.css">
 </head>
@@ -23,7 +23,7 @@
 <!-- ══ Nav ══ -->
 <nav class="navbar-muni d-flex align-items-center justify-content-between">
 
-    <a href="index.php" class="titulo-muni">Municipalidad</a>
+    <a href="index.php" class="titulo-muni">SGISC</a>
 
     <div class="d-flex align-items-center gap-2">
         <?php if (isset($_SESSION["usuario"])): ?>
