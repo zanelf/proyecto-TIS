f39e14c28352e730ad7bc5a19cba1503a5d497c2
Jayaquelo <fvidela@ing.ucsc.cl>
2026-06-18 02:21:12 -0400

    Cambio de título

--- web-app/ventanas/Enviarsolicitud.php
+++ web-app/ventanas/Enviarsolicitud.php
@@ -23,7 +23,7 @@
 <!-- ══ Nav ══ -->
 <nav class="navbar-muni d-flex align-items-center justify-content-between">
 
-    <a href="../index.php" class="titulo-muni">Municipalidad</a>
+    <a href="../index.php" class="titulo-muni">SGISC</a>
 
     <div class="d-flex align-items-center gap-2">
         <?php if (isset($_SESSION["usuario"])): ?>
