d0c7b2e36c798012a3866a356e8be95eb9459238
Jayaquelo <fvidela@ing.ucsc.cl>
2026-07-06 20:19:27 -0400

    Correción rutas en index.php e incorporación JS a métricas.php

--- web-app/index.php
+++ web-app/index.php
@@ -12,8 +12,8 @@ session_start();
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="../recursos/css/style_index.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="../recursos/css/style_metricas.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="recursos/css/style_index.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="recursos/css/style_metricas.css?v=<?php echo time(); ?>">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
