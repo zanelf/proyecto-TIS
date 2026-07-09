6d750b5889e00cd7bfb67df2c8e7785321bb4144
Aesedefe1 <67512673+Aesedefe1@users.noreply.github.com>
2026-07-09 11:34:58 -0600

    autentificacion arreglo

--- web-app/consultas/Autentificacion.php
+++ web-app/consultas/Autentificacion.php
@@ -7,11 +7,10 @@
 
     $consulta = "SELECT * FROM usuario WHERE rut_usuario='$usuario' LIMIT 1";
     $resultado = mysqli_query($conexionDB, $consulta);
-
     if(mysqli_num_rows($resultado) > 0){
         $datosUsuario = mysqli_fetch_assoc($resultado);
 
-        if ((int)$datosUsuario["Activo"] === 0) {
+        if ((int)$datosUsuario["activo"] === 0) {
             header('Location: ../ventanas/Inicio.php?error=inactivo');
             exit;
         }
@@ -23,47 +22,32 @@
             $_SESSION["ID_usuario"] = $datosUsuario["ID_usuario"];
             
             $var = $_SESSION["ID_usuario"];
-
-
-            $adm = "SELECT * FROM administrador WHERE ID_usuario='$var';";
-            $esAdm = mysqli_query($conexionDB, $adm);
-
-            $func = "SELECT * FROM funcionario WHERE ID_usuario='$var';";
-            $esFun = mysqli_query($conexionDB, $func);
-
-            $dir = "SELECT * FROM director WHERE ID_usuario='$var';";
-            $esDir = mysqli_query($conexionDB, $dir);
-
-            $dev = "SELECT * FROM desarrollador WHERE ID_usuario='$var';";
-            $esDev = mysqli_query($conexionDB, $dev);
             
+            $consulta="SELECT * FROM Trabajador WHERE ID_usuario = '$var'";
+            $datosTrabajador=mysqli_fetch_assoc(mysqli_query($conexionDB,$consulta));
+            
+            $datoDpto= $datosTrabajador['ID_departamento'];
+            $datoTipo= $datosTrabajador['tipo_trabajador'];
 
-            if(mysqli_num_rows($esAdm) > 0){
+            if((int)$datosUsuario["is_admin"]==1){
                 $_SESSION["ID_departamento"] = "";
                 $_SESSION["tipo"] = "Administrador";
-            }
-            elseif(mysqli_num_rows($esFun) > 0){ 
-                $consulta="SELECT ID_departamento FROM Funcionario WHERE ID_usuario = '$var'; ";
+                $_SESSION["ID_departamento"] = $datoDpto;
+            }elseif($datosTrabajador["tipo_trabajador"]==="Funcionario"){ 
                 $_SESSION["ID_departamento"] = mysqli_fetch_assoc(mysqli_query($conexionDB,$consulta))['ID_departamento'];
                 $_SESSION["tipo"] = "Funcionario";
             }
-            elseif(mysqli_num_rows($esDir) > 0){
+            elseif($datosTrabajador["tipo_trabajador"]==="Director"){
                 $_SESSION["tipo"] = "Director";
-            }
-            elseif(mysqli_num_rows($esDev) > 0){
-                
-                $_SESSION["tipo"] = "Desarrollador";
+                $_SESSION["ID_departamento"] = mysqli_fetch_assoc(mysqli_query($conexionDB,$consulta))['ID_departamento'];
             }
 
-            if (isset($_SESSION["tipo"])) {
-                switch ($_SESSION["tipo"]) {
-                    case "Administrador":
-                        header('Location: ../ventanas/Usuario.php');
-                        break;
-                    case "Director":
+            if ($datoTipo) {
+                switch ($datoTipo) {
+                    case "director":
                         header('Location: ../ventanas/director.php');
                         break;
-                    case "Funcionario":
+                    case "funcionario":
                         header('Location: ../ventanas/solicitud.php');
                         break;
                     default:
