952cc58c7d7e0c24cc8ca5c2ba51845396167146
zanelf <63268578+zanelf@users.noreply.github.com>
2026-07-09 12:44:32 -0400

    prueba de DB-v7

--- web-app/base_de_datos/DB-v7.sql
+++ web-app/base_de_datos/DB-v7.sql
@@ -3,13 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2026 at 10:22 AM
+-- Generation Time: Jul 09, 2026 at 06:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -18,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `proyectodb2-2`
+-- Database: `proyectodb`
 --
 
 -- --------------------------------------------------------
