49f9c318e1ae99cb03af9b81084a14924181cc83
Aesedefe1 <67512673+Aesedefe1@users.noreply.github.com>
2026-07-08 23:09:00 -0600

    arreglo revision y sql

--- web-app/consultas/CambiarEstadoSol.php
+++ web-app/consultas/CambiarEstadoSol.php
@@ -4,16 +4,21 @@
 
     $IDsol=$_POST['ID_cambio'];
     $estadoSiguiente=$_POST['estadoSiguiente'];
-
-    echo $IDsol;
-    echo $estadoSiguiente;
-
+    $prioridad= $_POST['ID_prioridad'];
+    $dpto= $_POST['ID_departamento'];
+    $IDtipoSol=$solicutdRevisar['ID_tipo_solicitud'];
     $consulta="UPDATE solicitud SET Tipo_estado = '$estadoSiguiente' WHERE solicitud_ID='$IDsol'";
     $resultado=mysqli_query($conexionDB,$consulta);
-    $consulta="SELECT tiempo FROM tiempo WHERE "
+
+    $consulta="SELECT tiempo FROM tiempo WHERE $IDtipoSol";
 
     if($estadoSiguiente=="Derivada"){
-        $consulta="SELECT tiempo FROM tiempo WHERE "
+        $consulta="SELECT tiempo FROM tiempo WHERE ID_prioridad='$prioridad' and ID_departamento = '$dpto' and ID_tipo_solicitud='$IDtipoSol'";
+        $resultado=mysqli_query($conexionDB,$consulta);
+        $tiempoAsignado=mysqli_fetch_assoc($resultado)['Tiempo'];
+        $consulta2="UPDATE solicitud SET Tiempo_asignado = '$tiempoAsignado'";
+    }
+    if($estadoSiguiente=="Respondida"){
     }
     
     if(!isset($_SESSION["usuario"])){
